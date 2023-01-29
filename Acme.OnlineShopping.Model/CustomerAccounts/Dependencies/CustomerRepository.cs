08e79023f0d4d47f6287090d0e4dd7897837048f
vijay@envivo <vijay.patel@envivo.co.uk>
2023-01-29 22:24:44 +0000

    fix: Added CustomerRepository, to allow Customers to be 'saved' via the UI

---
+++ Acme.OnlineShopping.Model/CustomerAccounts/Dependencies/CustomerRepository.cs
@@ -0,0 +1,69 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelTypes;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Acme.OnlineShopping.CustomerAccounts.Dependencies
+{
+    /// <summary>
+    /// Repository for managing Customers in a data store
+    /// </summary>
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private static readonly InMemoryRepository<Customer> _InMemoryRepository = new();
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<Customer> GetAll()
+        {
+            return _InMemoryRepository.GetAll();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public Customer? Load(Guid id)
+        {
+            return _InMemoryRepository.Load(id);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public int Save(Customer Customer, IEnumerable<object> newObjects, IEnumerable<object> modifiedObjects, IEnumerable<object> deletedObjects)
+        {
+            return _InMemoryRepository.Save(Customer, newObjects, modifiedObjects, deletedObjects);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public void Delete(Customer Customer)
+        {
+            _InMemoryRepository.Delete(Customer);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public IAggregateLock? Lock(Customer Customer)
+        {
+            return _InMemoryRepository.Lock(Customer);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public void Unlock(Customer Customer)
+        {
+            _InMemoryRepository.Unlock(Customer);
+        }
+    }
+}
